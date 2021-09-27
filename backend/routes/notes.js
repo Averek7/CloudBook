c31ce0e2bced38b1bf26022e969dc34fcb03b460
Derek <singhavi2122@gmail.com>
2021-09-27 22:18:54 +0530

    note commit

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -17,14 +17,15 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 })
 
 
-// Route 1: - Adding new notes via POST "api/notes/addnotes".... Login Required !
-router.get('/addnotes', fetchuser, [
+// Route 2: - Adding new notes via POST "api/notes/addnotes".... Login Required !
+router.post('/addnotes', fetchuser, [
 
     // Express Validator
     body('title', 'Enter a valid title').isLength({ min: 5}),
     body('description', 'Enter a valid description').isLength({ min: 10}),
 
 ], async (req, res) => {
+    const {title, description, tag} = req.body;
 
     // if there are errors, return a Bad Request and the errors
     const errors = validationResult(req);
@@ -33,15 +34,48 @@ router.get('/addnotes', fetchuser, [
     }
 
 
-    try {
-        const notes = await Notes.find({user : req.user.id});
-        res.json({notes});
+    try {        
+        const notes = new Notes({
+            title, description, tag, user : req.user.id
+        })
+        const savedNote = await notes.save();
+        res.json(savedNote);
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Some error occurred");
     }
 })
 
+// Route 3: - Update note via PUT "api/notes/updatenotes".... Login Required !
+router.put('/updatenotes/:id', fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    // Create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
 
+    //Checks whether the user is associated with the specified notes 
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found !")}
+
+    if(note.user.toString() !== req.user.id){return res.status(401).send("Access Denied !")}
+    
+    // Find the note to be updated
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+    res.json({note})
+})
+
+router.put('/deletenotes/:id', fetchuser, async (req, res) => {
+ 
+    //Checks whether the user is associated with the specified notes 
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found !")}
+
+    if(note.user.toString() !== req.user.id){return res.status(401).send("Access Denied !")}
+
+    note = await Notes.findByIdAndDelete(req.params.id, note);
+    res.json({note});
+});
 
 module.exports = router
